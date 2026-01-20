c47d7a950bac7caf8cf0c7b76c3d4200e411e15e
Jubair <jubairbro@users.noreply.github.com>
2026-01-20 13:12:20 +0600

    Rebrand: Changed App Name and ID, kept internal code safe

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.follow.clash"
+        applicationId = "com.sensei.clash"
         minSdk = flutter.minSdkVersion
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = flutter.versionCode
